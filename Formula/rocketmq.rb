eebf0fa9f91405bccb5b36948a8709ee3fc6e0ab
Jianhai Xu <jianhaixu@Jianhais-MacBook-Pro.local>
2020-02-16 17:31:05 +0800

    feat: remove latest version

--- Formula/rocketmq.rb
+++ Formula/rocketmq.rb
@@ -4,7 +4,6 @@
 class Rocketmq < Formula
   desc "Apache RocketMQ is a distributed messaging and streaming platform with low latency, high performance and reliability, trillion-level capacity and flexible scalability."
   homepage "https://rocketmq.apache.org/"
-  version "4.6.1"
   url "http://mirror.bit.edu.cn/apache/rocketmq/4.6.0/rocketmq-all-4.6.0-bin-release.zip"
   sha256 "584910d50639297808dd0b86fcdfaf431efd9607009a44c6258d9a0e227748fe"
 
